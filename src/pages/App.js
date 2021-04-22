afa56195f3c0655ef534ecf61a75f772754862b0
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-04-22 16:10:57 -0400

    ADD: Estilos y archivo del Main agregado
    Agregado:
    - Estilos del Hero
    - Estilos en general
    - Archivo del Main

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
 import Hero from "../components/Hero";
+import Main from "./Main";
 
 const App = () => {
   return (
     <Fragment>
       <Hero />
+      <Main />
     </Fragment>
   );
 };
