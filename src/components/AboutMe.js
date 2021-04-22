408777c54526fb7d1d06a2a8e334175cee5e74f9
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-04-22 16:57:53 -0400

    ADD: Agregando el About Me

---
+++ src/components/AboutMe.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+import "./css/AboutMe.css";
+
+const AboutMe = () => {
+  return (
+    <section className="aboutme">
+      <h2 className="title">About Me</h2>
+      <p className="intro">
+        👨‍💻 Programador FrontEnd, dedicado al mundo Tech 💙💻 mientras no para de
+        aprender 🤓 y compartir su conocimiento con otros.
+      </p>
+      <div className="photo">
+        <img src="https://i.imgur.com/SImAnNX.jpg" alt="Foto del Usuario" />
+      </div>
+      <p className="phrase">
+        <i>Nunca te rindas y supera tus limites </i>💪🔥
+      </p>
+    </section>
+  );
+};
+
+export default AboutMe;
