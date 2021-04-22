1cbcaf8f466123769c8660b8f091a7e7b6855550
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-04-22 15:56:05 -0400

    ADD: Pasando el Hero a React

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <header className="hero">
+      <div className="profile-name">
+        <h1>Luis Rodriguez</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Hero;
---
+++ src/pages/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import Hero from "../components/Hero";
+
+const App = () => {
+  return (
+    <Fragment>
+      <Hero />
+    </Fragment>
+  );
+};
+
+export default App;
