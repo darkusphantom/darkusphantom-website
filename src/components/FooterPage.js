cd215a692b593b8e46026db7be919599a25351fb
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-05-10 16:39:03 -0400

    ADD: FooterPage
    - Estructura y estilos

---
+++ src/components/FooterPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./css/FooterPage.css";
+
+function FooterPage() {
+  return (
+    <footer>
+      <p>
+        Copyright &copy; | Creado y diseñado por mi,
+        <i>Darkus Phantom (Luis Rodriguez)</i>🌟
+      </p>
+    </footer>
+  );
+}
+
+export default FooterPage;
