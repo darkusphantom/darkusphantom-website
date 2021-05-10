84c14e18630f2c00637538ace2336298b1aa1f65
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-05-10 15:09:45 -0400

    ADD: Seccion de Proyectos (Portafolio)
    - Le quite algunos proyectos. Dejando la estructura para 3
    - Agregue un archivo Data.js que de ahi sacara los datos de referencia para
    mostrar los enlaces del proyecto

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,32 @@
 import React from "react";
 
 import "./css/Projects.css";
+import ListProjects from "../data/ListProjects";
 
 function Projects() {
   return (
     <section id="projects">
       <h2 className="title">Proyectos Recientes</h2>
       <div className="project-list">
+        {ListProjects.map(
+          ({ id, name, projectPage, projectCover, projectCodeLink }) => {
+            return (
+              <div key={id} className="project">
+                <figure className="project-img">
+                  <img src={projectCover} alt="Project Imagen" />
+                </figure>
+              </div>
+            );
+          }
+        )}
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
+
+/*
         <a
           href="https://darkusphantom.github.io/CSS-Grid-Layout/"
           className="project"
@@ -20,9 +40,6 @@ function Projects() {
             </p>
           </span>
         </a>
-      </div>
-    </section>
-  );
-}
 
-export default Projects;
+
+*/
---
+++ src/data/ListProjects.js
@@ -0,0 +1,49 @@
+const ListProjects = [
+  {
+    id: 1,
+    name: "Signup Page",
+    projectCodeLink: "https://github.com/DarkusPhantom/signup-todo-app",
+    projectPage: "https://tasklist-todoapp.netlify.app/",
+    projectCover: "https://i.imgur.com/tr5fGmE.jpg",
+  },
+  {
+    id: 2,
+    name: "Landing Page",
+    projectCodeLink:
+      "https://github.com/DarkusPhantom/landing-page-con-unica-seccion-de-introduccin",
+    projectPage:
+      "https://darkusphantom.github.io/landing-page-con-unica-seccion-de-introduccin",
+    projectCover: "https://i.imgur.com/tr5fGmE.jpg",
+  },
+  {
+    id: 3,
+    name: "Preview Page of an Article",
+    projectCodeLink:
+      "https://github.com/DarkusPhantom/reto-componente-de-vista-previa-de-articulo",
+    projectPage:
+      "https://darkusphantom.github.io/reto-componente-de-vista-previa-de-articulo/",
+    projectCover: "https://i.imgur.com/tr5fGmE.jpg",
+  },
+  /*  {
+    id: 4,
+    name: "PlatziVideo",
+    projectCodeLink: "https://darkusphantom.github.io/CSS-Grid-Layout/",
+    projectPage:"",
+    projectCover: "https://i.imgur.com/tr5fGmE.jpg",
+  },*/
+];
+
+export default ListProjects;
+/*<!--
+Signup-TodoAPp
+code: https://github.com/DarkusPhantom/signup-todo-app
+page: https://tasklist-todoapp.netlify.app/
+
+Landing page:
+code: https://github.com/DarkusPhantom/landing-page-con-unica-seccion-de-introduccin
+page: https://darkusphantom.github.io/landing-page-con-unica-seccion-de-introduccin/
+
+Vista previa de un articulo:
+code: https://github.com/DarkusPhantom/reto-componente-de-vista-previa-de-articulo
+page: https://darkusphantom.github.io/reto-componente-de-vista-previa-de-articulo/
+-->*/
