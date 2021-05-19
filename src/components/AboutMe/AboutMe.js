300e0d0ea8070122e827bbf566f4446cf62b4bad
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-05-18 22:48:59 -0400

    REFACTOR:
    - Mejorando el nombre de variables en las clases
    - Se agrego una descripcion profesional a mi perfil
    - Se cambio la descripcion basica
    - Se reestructuro el codigo a base de cajitas

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -5,18 +5,55 @@ import "./AboutMe.css";
 const AboutMe = () => {
   return (
     <section className="aboutme space-section">
+      <h2 className="title-section">Acerca de Mi</h2>
       <div className="container-grid">
-        <h2 className="title-section">About Me</h2>
-        <p className="intro">
-          👨‍💻 Programador FrontEnd, dedicado al mundo Tech 💙💻 mientras no para
-          de aprender 🤓 y compartir su conocimiento con otros.
-        </p>
-        <div className="photo">
-          <img src="https://i.imgur.com/SImAnNX.jpg" alt="Foto del Usuario" />
+        <div className="section-content aboutme-photo container-img">
+          <img
+            className="container-img-item"
+            src="https://i.imgur.com/SImAnNX.jpg"
+            alt="Foto del Usuario"
+          />
         </div>
-        <p className="phrase">
-          <i>Nunca te rindas y supera tus limites </i>💪🔥
-        </p>
+        <div className="section-content info-programmer">
+          <h3>
+            👨‍💻 FrontEnd
+            <br />
+            Developer
+          </h3>
+          <p>
+            Creo proyectos personales como desarrollador; mejorando mis
+            conocimientos, habilidades como programador y creación de
+            herramientas.
+          </p>
+        </div>
+        <div className="section-content info-blogger">
+          <h3>✍️ Blogger</h3>
+          <p>
+            Escribo en un blog para compartir lo que aprendo, de desarrollo
+            personal, tecnología o desarrollo web, para informar, aportar valor
+            y motivar a otros.
+          </p>
+        </div>
+        <div className="section-content info-phrase">
+          <p>
+            🌟 <i>Nunca te rindas y supera tus limites</i> 🌟
+          </p>
+        </div>
+      </div>
+      <div class="container">
+        <div class="section-content info-description">
+          <p>
+            Me fascina la tecnología y todo lo referente a la misma. Me gusta
+            estar en constante aprendizaje, saber cómo funcionan las cosas y el
+            por qué. Busco ayudar a otros creando contenido y escribiendo acerca
+            de tecnología, desarrollo web y desarrollo personal en mi{" "}
+            <strong>Marca Personal</strong>.<br /> En cada objetivo que me
+            propongo, busco superarme. Quiero sacar la mejor version mi, tanto a
+            nivel personal como profesional, y alcanzar mis metas mientras
+            impulso, inspiro y motivo, a otros, con lo que aprendo y mis
+            experiencias.
+          </p>
+        </div>
       </div>
     </section>
   );
