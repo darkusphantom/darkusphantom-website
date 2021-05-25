191beaa10d290cbdc69c2aa6c0acc04dc4a754e4
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-05-24 20:28:05 -0400

    REFACTOR: Seccion para el Blog
    - La seccion de Blog que estaba en Contacts se ha movido a la
    linea principal del website

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -4,6 +4,7 @@ import "../../index.css";
 import "./Main.css";
 import AboutMe from "../../components/AboutMe/AboutMe";
 import Projects from "../../components/Projects/Projects";
+import Blog from "../../components/Blog/blog";
 import SocialNetwork from "../../components/SocialNetworks/SocialNetworks";
 import FooterPage from "../../components/FooterPage/FooterPage";
 
@@ -13,6 +14,7 @@ const Main = () => {
       <div className="container">
         <AboutMe />
         <Projects />
+        <Blog />
         <SocialNetwork />
         <FooterPage />
       </div>
