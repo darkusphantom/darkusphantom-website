d3f6502ec1e9b9819065a9410877d894233565fc
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-05-27 17:34:53 -0400

    FEATURE: Lenguajes de Programacion
    Se añadio los lenguajes de programacion que domino como programador
    a la seccion de AboutMe

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./AboutMe.css";
 import AboutMeIntro from "../AboutMeIntro/AboutMeIntro";
 import AboutMeDescription from "../AboutMeDescription/AboutMeDescription";
+import ProgrammingLanguaje from "../ProgrammingLanguaje/ProgrammingLanguaje";
 
 const AboutMe = () => {
   return (
@@ -13,6 +14,7 @@ const AboutMe = () => {
           <AboutMeIntro />
         </div>
         <AboutMeDescription />
+        <ProgrammingLanguaje />
       </div>
     </section>
   );
---
+++ src/components/ProgrammingLanguaje/ProgrammingLanguaje.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+import "./ProgrammingLanguaje.css";
+
+const ProgrammingLanguaje = () => {
+  const programmingLanguajes = [
+    {
+      id: 1,
+      name: "HTML",
+    },
+    {
+      id: 2,
+      name: "CSS",
+    },
+    {
+      id: 3,
+      name: "Javascript",
+    },
+    {
+      id: 4,
+      name: "ReactJS",
+    },
+    {
+      id: 5,
+      name: "Webpack",
+    },
+    {
+      id: 6,
+      name: "NPM",
+    },
+  ];
+
+  return (
+    <div className="aboutme__programmingLanguaje container-grid">
+      {programmingLanguajes.map((languaje) => {
+        return (
+          <div className="box__programmingLanguaje" key={languaje.id}>
+            <span className="programmingLanguaje">{languaje.name}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ProgrammingLanguaje;
