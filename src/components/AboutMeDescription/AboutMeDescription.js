573619cf837acf438d0217b6ea867596a9ee2722
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-05-28 16:08:46 -0400

    REFACTOR: Los datos de los lenguajes se movieron a data
    ADD: Animaciones a los lenguajes en version movil
    - Animacion fadeIn para la version movil
    
    FIX: Vista en diseño moviles
    - Se agrego un espaciado (br) en el parrafo descriptivo
    AboutMe
    - Se centro los lenguajes y se arreglo la caja para que
    las palabras no se salieran. Tambien se le agrego negrita

--- src/components/AboutMeDescription/AboutMeDescription.js
+++ src/components/AboutMeDescription/AboutMeDescription.js
@@ -8,6 +8,7 @@ const AboutMeDescription = () => {
       <p className="aboutme__text aboutme__text__description">
         Me fascina la tecnología y todo lo referente a la misma. Me gusta estar
         en constante aprendizaje, saber cómo funcionan las cosas y el por qué.
+        <br />
         Busco ayudar a otros creando contenido y escribiendo acerca de
         tecnología, desarrollo web y desarrollo personal en mi{" "}
         <strong>Marca Personal</strong>.
--- src/components/ProgrammingLanguaje/ProgrammingLanguaje.js
+++ src/components/ProgrammingLanguaje/ProgrammingLanguaje.js
@@ -1,39 +1,13 @@
 import React from "react";
 
 import "./ProgrammingLanguaje.css";
+import ProgrammingLanguajes from "../../data/ProgrammingLanguajes";
 
 const ProgrammingLanguaje = () => {
-  const programmingLanguajes = [
-    {
-      id: 1,
-      name: "HTML",
-    },
-    {
-      id: 2,
-      name: "CSS",
-    },
-    {
-      id: 3,
-      name: "Javascript",
-    },
-    {
-      id: 4,
-      name: "ReactJS",
-    },
-    {
-      id: 5,
-      name: "Webpack",
-    },
-    {
-      id: 6,
-      name: "NPM",
-    },
-  ];
-
   return (
     <div className="aboutme__programmingLanguaje container-grid">
       <h3 className="aboutme__title">Mis Herramientas</h3>
-      {programmingLanguajes.map((languaje) => {
+      {ProgrammingLanguajes.map((languaje) => {
         return (
           <div className="box__programmingLanguaje" key={languaje.id}>
             <span className="programmingLanguaje">{languaje.name}</span>
