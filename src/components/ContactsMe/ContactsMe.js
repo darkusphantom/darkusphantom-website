84c7b796acdb2791cf739e2942795a977efdeab0
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-05-28 14:41:43 -0400

    REFACTOR: Division en Componentes de SocialNetwork - Se dividio la seccion de SocialNetworks en SocialNetworkElements y ContactsMe
    FIX: Diseño en Version Movil
    
    ADD: Border a las imagenes en general
    
    DELETE: Comentarios

---
+++ src/components/ContactsMe/ContactsMe.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import "./ContactsMe.css";
+
+const ContactsMe = () => {
+  return (
+    <section className="contacts__contactme container-grid">
+      <h3 className="contacts__title">¿Buscas a alguien con quien trabajar?</h3>
+      <p className="contacts__text">
+        Si buscas a alguien para colaborar, o practicar, en algun proyecto, las
+        puertas están abiertas. <br />
+        Con mucho gusto estaría encantado de trabajar contigo.
+      </p>
+      <a
+        href="https://t.me/darkusphantomchannel"
+        className="contacts__btn btn btn-primary"
+      >
+        👉 Contáctame aqui 👈
+      </a>
+      <div className="contacts__image container-img">
+        <img
+          className="container-img-item"
+          src="https://i.imgur.com/mhUo1hK.png"
+          alt="Meme indicando que Trabajemos en Equipo para colaborar con alguien o, trabajar en una empresa o startup"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default ContactsMe;
