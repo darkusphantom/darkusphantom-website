0da815bd378ede4d56836642a1d4b43a8ec03707
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-05-29 15:41:09 -0400

    ADD: Imagen para mi descripcion
    Se añadio una imagen para rellenar espacion en mis descripciones

--- src/components/AboutMeDescription/AboutMeDescription.js
+++ src/components/AboutMeDescription/AboutMeDescription.js
@@ -4,7 +4,7 @@ import "./AboutMeDescription.css";
 
 const AboutMeDescription = () => {
   return (
-    <div className="aboutme__content info__description container">
+    <div className="aboutme__content info__description container-grid">
       <p className="aboutme__text aboutme__text__description">
         Me fascina la tecnología y todo lo referente a la misma. Me gusta estar
         en constante aprendizaje, saber cómo funcionan las cosas y el por qué.
@@ -19,6 +19,13 @@ const AboutMeDescription = () => {
         metas mientras impulso, inspiro y motivo, a otros, con lo que aprendo y
         mis experiencias.
       </p>
+      <div className="container-img aboutme__img">
+        <img
+          className="container-img-item"
+          src="https://images.unsplash.com/photo-1484807352052-23338990c6c6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHBlcnNvbmFsJTIwZ3Jvd3RofGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+          alt="Persona sentada utilizando una laptop"
+        />
+      </div>
     </div>
   );
 };
